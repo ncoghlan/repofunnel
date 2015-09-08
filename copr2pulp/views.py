3be7dc98204f3bd2ddb99dae529896f178039927
Nick Coghlan <ncoghlan@gmail.com>
2015-09-08 17:13:00 +1000

    First table populated from REST API

--- copr2pulp/views.py
+++ copr2pulp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from . import coprapi
 from . import pulpapi
 
@@ -6,8 +7,11 @@ from . import pulpapi
 def repo_overview(request):
     # Get remote repo info from COPR
     sources = coprapi.get_repos()
+    copr_api_url = reverse("copr_repo-list")
     # Get local repo info from Pulp
     targets = pulpapi.get_repos()
     # Display them both
-    context = {"remote_repos": sources, "local_repos": targets}
+    context = {"remote_repos": sources,
+               "remote_repo_url": copr_api_url,
+               "local_repos": targets}
     return render(request, 'copr2pulp/sourcelist.html', context=context)
