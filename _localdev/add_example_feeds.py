f38b2756a0d1519be13474c418f0f3039532ee8b
Nick Coghlan <ncoghlan@redhat.com>
2015-09-24 17:48:45 +1000

    Add a helper script to populate feed repos

---
+++ _localdev/add_example_feeds.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+
+example_feeds = [
+    {
+        "name": "SCL-PostgreSQL94-EPEL-7-x86_64",
+        "feed_url": "https://copr-be.cloud.fedoraproject.org/results/rhscl/rh-postgresql94/epel-7-x86_64/"
+    },
+    {
+        "name": "SCL-Python34-EPEL-7-x86_64",
+        "feed_url": "https://copr-be.cloud.fedoraproject.org/results/rhscl/rh-python34-el7/epel-7-x86_64/"
+    },
+    {
+        "name": "PyPA-EPEL-7-x86_64",
+        "feed_url": "https://copr-be.cloud.fedoraproject.org/results/pypa/pypa/epel-7-x86_64/"
+    },
+]
+
+repofunnel_feeds_url = "http://localhost:8000/api/feed/"
+
+for feed_settings in example_feeds:
+    print(feed_settings)
+    reply = requests.post(repofunnel_feeds_url, json=feed_settings)
+    reply.raise_for_status()
