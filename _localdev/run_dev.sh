f69d523394b13cf6f446113366ad1a927e27f9b5
Nick Coghlan <ncoghlan@gmail.com>
2015-09-25 18:26:59 +1000

    Use the Werkzeug interactive debugger in dev

--- _localdev/run_dev.sh
+++ _localdev/run_dev.sh
@@ -6,4 +6,4 @@ docker run -it --rm --name repofunnel_dev --link pulpapi:pulpapi -p 8000:8000 \
            pip3 install -r '/opt/repofunnel/requirements.txt' && \
            /opt/repofunnel/manage.py makemigrations && \
            /opt/repofunnel/manage.py migrate && \
-           /opt/repofunnel/manage.py runserver 0.0.0.0:8000"
+           /opt/repofunnel/manage.py runserver_plus 0.0.0.0:8000"
